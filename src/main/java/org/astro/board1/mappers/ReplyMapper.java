fb58f5622ecc0ab329608c0b26e32b2f120e84cc
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-08-03 21:39:46 +0900

    230803 대댓글 처리중

--- src/main/java/org/astro/board1/mappers/ReplyMapper.java
+++ src/main/java/org/astro/board1/mappers/ReplyMapper.java
@@ -14,11 +14,21 @@ public interface ReplyMapper {
     //total
     int getBnoCount();
 
+    /* gno가 0일 때 실행하는 영역 */
+    //register 댓글
+    int registerReply(ReplyDTO replyDTO);
+
+    // 댓글의 gno 업데이트
+    int updateReplyGno(Integer gno);
+    /* //gno가 0일 때 실행하는 영역 */
+
+    /* gno가 0이 아닐 때 실행하는 영역 */
+    //register 대댓글
+    int registerReplyChild(ReplyDTO replyDTO);
+    /* //gno가 0이 아닐 때 실행하는 영역 */
+
     //read
     ReplyDTO selectOne(Integer rno);
-    
-    //등록
-    int register(ReplyDTO replyDTO);
 
     //수정
     int modifyReply(ReplyDTO replyDTO);
